fed8bd7686e6bf4eb8e132c0becd8541b462e948
jiangchen <cjiang@86links.com>
2021-02-05 15:59:49 +0800

    合并有序数组

--- src/tech/jiangchen/array/MergeTwoSortedArray.java
+++ src/tech/jiangchen/array/MergeTwoSortedArray.java
@@ -10,42 +10,31 @@ public class MergeTwoSortedArray {
 
     /**
      * 合并两个有序数组
-     *
-     * @param a
-     * @param b
-     * @return
      */
 
-    public void mergeTwoLists(int[] a, int al, int[] b, int bl) {
-        int[] result = new int[al + bl];
-        int ai = 0;
-        int bi = 0;
-        int i = 0;
-        while (ai < al && bi < bl) {
-            if (a[ai] <= b[bi]) {
-                result[i++] = a[ai++];
-            } else {
-                result[i++] = b[bi++];
-            }
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m - 1;
+        int len2 = n - 1;
+        int len = m + n - 1;
+        while (len1 >= 0 && len2 >= 0) {
+            // 注意--符号在后面，表示先进行计算再减1，这种缩写缩短了代码
+            nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
         }
-        if (ai == al) {
-            for (int k = bi; k < bl; k++) {
-                result[i++] = b[k];
-            }
-        } else {
-            for (int k = ai; k < al; k++) {
-                result[i++] = a[k];
-            }
-        }
+        // 表示将nums2数组从下标0位置开始，拷贝到nums1数组中，从下标0位置开始，长度为len2+1
+        System.arraycopy(nums2, 0, nums1, 0, len2 + 1);
 
-        a = result;
-        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+
+        System.out.println(Arrays.toString(nums1));
     }
 
     public static void main(String[] args) {
-        int[] a = new int[]{1, 3, 5, 7};
-        int[] b = new int[]{2, 4, 6};
+        int[] a = new int[]{1, 3, 5, 0, 0};
+        int[] b = new int[]{2, 4};
         MergeTwoSortedArray demo = new MergeTwoSortedArray();
-        demo.mergeTwoLists(a, a.length, b, b.length);
+        demo.merge1(a, a.length - b.length, b, b.length);
     }
 }
