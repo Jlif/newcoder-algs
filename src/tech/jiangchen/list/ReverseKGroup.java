e7ba98304ba9539ed3841e7f9146023fe12e9ad3
jiangchen <cjiang@86links.com>
2021-06-25 18:23:22 +0800

    k个一组反转链表

--- src/tech/jiangchen/list/ReverseKGroup.java
+++ src/tech/jiangchen/list/ReverseKGroup.java
@@ -8,6 +8,24 @@ import tech.jiangchen.struct.ListNode;
  */
 public class ReverseKGroup {
 
+    ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        ListNode a = head;
+        ListNode b = head;
+        for (int i = 0; i < k; i++) {
+            if (b == null) {
+                return head;
+            }
+            b = b.next;
+        }
+        // 反转前 k 个元素
+        ListNode newHead = reverse(a, b);
+        // 递归反转后续链表并连接起来
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
 
     ListNode reverse(ListNode head) {
         ListNode pre = null;
